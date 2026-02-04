58d5db845f562b45d0584008587724ea0c10610b
Трушкова Екатерина Сергеевна <eksetrushkova@edu.hse.ru>
2026-02-04 21:00:18 +0300

    all

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,36 +1,11 @@
-from src.models.popular_random import PopularRandom
-from src.models.user_knn import UserKNNModel
-from src.models.item_knn import ItemKNNModel
-from src.models.seq_knn import SeqKNN
-from src.models.ease_r import EASE_R
-from src.models.bpr_mf import BPR_MF
-from src.models.als_mf import ALSMF_sparse
 from src.models.ultragcn import UltraGCN
-from src.models.sgd_mf import MFSGD
 from src.models.lightgcn import LightGCN
-from src.models.random import RandomModel
-from src.models.pure_svd import PureSVDModel
 from src.models.sasrec import SASRecModel
-from src.models.gasatf import GASATF
 from src.models.sasrec import SASRecModel
 from src.models.joint_sasrec_ultragcn import JointSASRecUltraGCN
 
 __all__ = [
-    "PopularRandom",
-    "PopularRandom",
-    "ALSFM_sparse",
     "UltraGCN",
-    "MFSGD",
-    "LightGCN",
-    "BPR_MF",
-    "EASE_R",
-    "UserKNNModel",
-    "ItemKNNModel",
-    "SeqKNN",
-    "RandomModel",
-    "PureSVDModel",
-    "SASRecModel"
-    "GASATF",
     "SASRecModel",
     "JointSASRecUltraGCN"
 ]
